9e865272a6d794c7e12491d691289cea838a40b4
srivarshavarsha64-tech <srivarshavarsha64@gmail.com>
2026-04-15 15:24:28 +0530

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 @st.cache_resource
 def load_summerizer():
   return pipeline("summarization",model="sshleifer/distilbart-cnn-12-6")
-  st.title("AI Text Summarizer")
+st.title("AI Text Summarizer")
 st.write("Enter a long text below, and get a concise Summary!")
 long_text = st.text_area("Enter text to Summarizer:", height = 200)
 max_length = st.slider("Max Summary Length", min_value=50,
@@ -13,8 +13,9 @@ min_length = st.slider("Min Summary Length" , min_value=20,
 if st.button("Summarize"):
   if long_tex.strip():
     with st.spinner("Generating summary..."):
-      summary = summarizer(long_text,max_length,min_length=min_length, do_sample=False)
+      summary = summarizer(long_text,max_length
+                           ,min_length=min_length, do_sample=False)
       st.subheader("Summary:")
       st.success(summary[0]['summary_texe'])
   else:
-  st.warning("⚠️ Please enter some text to summarize.")
+    st.warning("⚠️ Please enter some text to summarize.")
